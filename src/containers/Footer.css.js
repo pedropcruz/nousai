e2043d84596a10c2d35b852ffc399fb4be7469b2
pedropcruz <pmcruz@impresa.pt>
2018-03-28 02:16:24 +0100

    added to footer copyright component

--- src/containers/Footer.css.js
+++ src/containers/Footer.css.js
@@ -5,7 +5,11 @@ const FooterCSS = styled.footer`
   width: calc(100% - 2em);                                       
   align-items: center;                               
   height: 64px;
-  background: ${variable.colors.whiteColor};                                      
+  background: ${variable.colors.whiteColor};       
+  flex-direction: column-reverse;
+  @media ${variable.media.sm}{
+    flex-direction: row;
+  }                               
 `;
 
 export default FooterCSS;
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import FooterCSS from './Footer.css';
 import {SocialLinks} from "../components/Footer/SocialLinks";
+import Copyright from "../components/Footer/Copyright";
 
 export class Footer extends Component{
     render(){
         return(
             <FooterCSS className="nousai__footer p2 fixed flex justify-between bottom-0 left-0 right-0">
                 <SocialLinks/>
+                <Copyright/>
             </FooterCSS>
         )
     }
