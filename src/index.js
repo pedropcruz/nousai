3988fa46f2cf57690ffbbe6597f276c886f6f877
pedropcruz <pedropcruz@outlook.pt>
2018-03-29 03:04:00 +0100

    add routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/Global.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export class Root extends Component{
+    render(){
+        return(
+            <Router>
+                <Route exact path="/" component={App}/>
+            </Router>
+        );
+    }
+}
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
