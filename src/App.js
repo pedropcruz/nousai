6caadb0eda75d6114843415acf8f2a0e14da5c62
pedropcruz <pedropcruz@outlook.pt>
2018-04-03 02:19:33 +0100

    add Switch with all the routes && change Arrow component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Header} from "./containers/Header";
 import {Footer} from "./containers/Footer";
 import Arrow from './components/NavigationArrows/Arrow';
+import {Switch, Route} from 'react-router-dom';
 
 class App extends Component {
 
@@ -17,17 +18,16 @@ class App extends Component {
         return (
             <div className="nousai_app">
                 <Header overlayMenu={this.state.showOverlayMenu}/>
-                <Arrow
-                    prevPage={{
-                        url: '/',
-                        name: 'Home'
-                    }}
-                    nextPage={{
-                        url:'/About',
-                        name: 'About'
-                    }}
-                />
+                <Arrow />
                 <Footer/>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home</h1>} />
+                    <Route exact path="/about" render={() => <h1>About</h1>} />
+                    <Route exact path="/pricing" render={() => <h1>Pricing</h1>} />
+                    <Route exact path="/casestudies" render={() => <h1>case studies</h1>} />
+                    <Route exact path="/newsletter" render={() => <h1>newsletter</h1>} />
+                    <Route exact path="/contact" render={() => <h1>Contact</h1>} />
+                </Switch>
             </div>
         );
     }
