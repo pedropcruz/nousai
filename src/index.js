940bc75a75f19add24776029c40a996db7a7c2c3
pedropcruz <pedropcruz@outlook.pt>
2018-04-03 02:15:40 +0100

    change Routes to Component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/Global.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 export class Root extends Component{
     render(){
         return(
             <Router>
-                <Route exact path="/" component={App}/>
+                <App/>
             </Router>
         );
     }
