f7378d73b686f8ec22ee61f5bb56ebe6a5a1bdef
pedropcruz <pedropcruz@outlook.pt>
2018-04-12 23:13:32 +0100

    added component on Home link && section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Header} from "./containers/Header";
 import {Footer} from "./containers/Footer";
 import Arrow from './components/NavigationArrows/Arrow';
 import {Switch, Route} from 'react-router-dom';
+import {Home} from "./containers/Home";
 
 class App extends Component {
 
@@ -20,14 +21,16 @@ class App extends Component {
                 <Header overlayMenu={this.state.showOverlayMenu}/>
                 <Arrow />
                 <Footer/>
-                <Switch>
-                    <Route exact path="/" render={() => <h1>Home</h1>} />
-                    <Route exact path="/about" render={() => <h1>About</h1>} />
-                    <Route exact path="/pricing" render={() => <h1>Pricing</h1>} />
-                    <Route exact path="/casestudies" render={() => <h1>case studies</h1>} />
-                    <Route exact path="/newsletter" render={() => <h1>newsletter</h1>} />
-                    <Route exact path="/contact" render={() => <h1>Contact</h1>} />
-                </Switch>
+                <section className="nousai_app--section absolute">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" render={() => <h1>About</h1>} />
+                        <Route exact path="/pricing" render={() => <h1>Pricing</h1>} />
+                        <Route exact path="/casestudies" render={() => <h1>case studies</h1>} />
+                        <Route exact path="/newsletter" render={() => <h1>newsletter</h1>} />
+                        <Route exact path="/contact" render={() => <h1>Contact</h1>} />
+                    </Switch>
+                </section>
             </div>
         );
     }
